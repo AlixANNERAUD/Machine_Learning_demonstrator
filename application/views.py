3c68f188394e51debe3d41085280bd90a8c248c2
Alix ANNERAUD <alix@anneraud.fr>
2024-10-30 22:41:35 +0100

    feature: Add track view

--- application/views.py
+++ application/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
-def home(request):
-    return HttpResponse("Money goes brrrr ...")
+def tracks(request):
+    tracks = settings.TRACKS
+
+    paginator = Paginator(tracks, 100)
+
+    page_number = request.GET.get("page")
+
+    page = paginator.get_page(page_number)
+
+    tracks_html = page.object_list.to_html()
+
+    return render(request, "tracks.html", {"tracks": tracks_html, "page": page})
+
