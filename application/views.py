be00ff805805605da97688750bced5aa31956b9f
Alix ANNERAUD <alix@anneraud.fr>
2024-10-31 20:29:38 +0100

    feature: Rename t-SNE to umap and stuff to views

--- application/views.py
+++ application/views.py
@@ -4,36 +4,38 @@ from django.conf import settings
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from sklearn.manifold import TSNE
 import plotly.express as px
-import umap
+import numpy
+#import umap
 
 
 # Create your views here.
-def tracks(request):
-    tracks = settings.TRACKS
+def tracks_view(request):
+    search = request.GET.get("search", "")
+    
+    if search == "":
+        tracks = settings.TRACKS
+    else:
+        tracks = settings.TRACKS[settings.TRACKS["track_name"].str.contains(search, case=False, na=False)]
 
     paginator = Paginator(tracks, 100)
 
-    page_number = request.GET.get("page")
+    page_number = request.GET.get("page", 1)
 
     page = paginator.get_page(page_number)
 
     tracks_html = page.object_list.to_html()
 
-    return render(request, "tracks.html", {"tracks": tracks_html, "page": page})
+    return render(request, "tracks.html", {"tracks": tracks_html, "page": page, "search": search})
 
+def umap_view(request):
+    print(settings.UMAP.head())
+    
+    figure = px.scatter_3d(settings.UMAP[["x", "y", "z"]], x="x", y="y", z="z", color=numpy.linspace(0, 1, settings.UMAP.shape[0]))
 
-def tsne(request):
+    figure.update_layout(
+        margin=dict(l=0, r=0, t=0, b=0),
+    )
 
-    tracks = settings.TRACKS[::50]
+    plot_html = figure.to_html(full_html=False, default_height="100%", default_width="100%")
 
-    numeric_columns = tracks.select_dtypes(include=["float64", "int64"]).dropna(axis=0)
-
-    print(numeric_columns.info())
-
-    tsne_results = umap.UMAP().fit_transform(numeric_columns)
-
-    figure = px.scatter(x=tsne_results[:, 0], y=tsne_results[:, 1])
-
-    plot_html = figure.to_html()
-
-    return render(request, "tsne.html", {"plot": plot_html})
+    return render(request, "umap.html", {"plot": plot_html})
