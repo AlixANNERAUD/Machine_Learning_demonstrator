7a14fe64cd8b452ebc344a6c75e9c4c648281973
Alix ANNERAUD <alix@anneraud.fr>
2024-10-31 20:28:51 +0100

    feature: Add trailing / to urls and rename tsne to umap

--- application/urls.py
+++ application/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("tracks", views.tracks, name="Tracks"),
-    path("tsne", views.tsne, name="TSNE"),
+    path("tracks/", views.tracks_view, name="Tracks"),
+    path("umap/", views.umap_view, name="UMAP"),
 ]
