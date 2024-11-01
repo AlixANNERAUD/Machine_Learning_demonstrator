34d3bb4fcd176b75fec80ae30b58211c23e2402b
Alix ANNERAUD <alix@anneraud.fr>
2024-11-01 01:49:51 +0100

    refactor: Migrate app configuration from project settings to app configuration

--- application/apps.py
+++ application/apps.py
@@ -1,6 +1,106 @@
 from django.apps import AppConfig
 
+import pandas
+import logging
+import dotenv
+import os
+import traceback
+
+def load_tracks(path : str):
+    logging.info("Loading tracks ...")
+    try:
+        return pandas.read_csv(path)
+    except FileNotFoundError:
+        logging.error("Tracks file not found")
+        raise FileNotFoundError
+    except Exception as e:
+        logging.error(f"Error loading tracks: {e}")
+        raise e
+
+def load_umap(tracks : pandas.DataFrame, path : str):
+    logging.info("Loading UMAP ...")
+    try:
+        return pandas.read_hdf(path, key="umap")
+    except FileNotFoundError:
+        import umap
+
+        logging.info("UMAP file not found, generating ...")
+
+        numeric_columns = TRACKS.select_dtypes(include=["float64", "int64"]).dropna(
+            axis=0
+        )
+
+        umap_model = umap.UMAP(
+            n_components=3, n_neighbors=10, min_dist=0.1, metric="correlation"
+        )
+
+        UMAP = umap_model.fit_transform(numeric_columns)
+
+        UMAP = pandas.DataFrame(UMAP, columns=["x", "y", "z"])
+        
+        UMAP.to_hdf(path, key="umap")
+
+        return UMAP
+    except Exception as e:
+        logging.error(f"Error loading UMAP: {e}")
+        raise e
+        
+
+
+class LoggingFormatterClass(logging.Formatter):
+    EMOJIS = {
+        logging.DEBUG: "🐛",
+        logging.INFO: "📝",
+        logging.WARNING: "⚠️",
+        logging.ERROR: "❌",
+        logging.CRITICAL: "🔥"
+    }
+
+    def __init__(self):
+        super().__init__("%(asctime)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+
+    def format(self, record):
+        emoji = self.EMOJIS.get(record.levelno, "")
+        record.msg = f"{emoji} - {record.msg}"
+        return super().format(record)
 
 class ApplicationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'application'
+
+
+
+    def load_environment_variables(self):
+        logging.info("Loading environment variables ...")
+        dotenv.load_dotenv()
+        
+        self.data_path = os.getenv("DATA_PATH")
+
+        if self.data_path is None:
+            logging.error("Data path not found")
+            raise ValueError("Data path not found")
+
+        self.spotify_client_id = os.getenv("SPOTIFY_CLIENT_ID")
+
+        if self.spotify_client_id is None:
+            logging.error("Spotify client ID not found")
+            raise ValueError("Spotify client ID not found")
+
+        self.spotify_client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+
+        if self.spotify_client_secret is None:
+            logging.error("Spotify client secret not found")
+            raise ValueError("Spotify client secret not found")
+
+    def ready(self):
+        logging.basicConfig(level=logging.INFO)
+        logging.getLogger().handlers[0].setFormatter(LoggingFormatterClass())
+
+        self.load_environment_variables()
+
+        self.tracks = load_tracks(f"{self.data_path}/tracks.csv")
+        
+        self.umap = load_umap(self.tracks, f"{self.data_path}/umap.hdf")
+
+        logging.info("Application ready ✅")
+
--- application/views.py
+++ application/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.conf import settings
+from django.apps import apps
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from sklearn.manifold import TSNE
 import plotly.express as px
 import numpy
-#import umap
+
+configuration = apps.get_app_config("application")
 
 
 # Create your views here.
 def tracks_view(request):
     search = request.GET.get("search", "")
-    
+
     if search == "":
-        tracks = settings.TRACKS
+        tracks = configuration.tracks
     else:
-        tracks = settings.TRACKS[settings.TRACKS["track_name"].str.contains(search, case=False, na=False)]
+        tracks = configuration.tracks[
+            configuration.tracks["track_name"].str.contains(
+                search, case=False, na=False
+            )
+        ]
 
     paginator = Paginator(tracks, 100)
 
@@ -25,15 +29,26 @@ def tracks_view(request):
 
     tracks_html = page.object_list.to_html()
 
-    return render(request, "tracks.html", {"tracks": tracks_html, "page": page, "search": search})
+    return render(
+        request, "tracks.html", {"tracks": tracks_html, "page": page, "search": search}
+    )
+
 
 def umap_view(request):
-    figure = px.scatter_3d(settings.UMAP[["x", "y", "z"]], x="x", y="y", z="z", color=numpy.linspace(0, 1, settings.UMAP.shape[0]))
+    figure = px.scatter_3d(
+        configuration.umap[["x", "y", "z"]],
+        x="x",
+        y="y",
+        z="z",
+        color=numpy.linspace(0, 1, configuration.umap.shape[0]),
+    )
 
     figure.update_layout(
         margin=dict(l=0, r=0, t=0, b=0),
     )
 
-    plot_html = figure.to_html(full_html=False, default_height="100%", default_width="100%")
+    plot_html = figure.to_html(
+        full_html=False, default_height="100%", default_width="100%"
+    )
 
     return render(request, "umap.html", {"plot": plot_html})
