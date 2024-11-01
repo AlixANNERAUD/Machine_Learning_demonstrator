a2c10a9609caf244b7c01ed4b926c7f3d89ca6dc
Alix ANNERAUD <alix@anneraud.fr>
2024-11-01 01:51:03 +0100

    feature: Add redirection from root to account

--- application/urls.py
+++ application/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.home_view, name="Home"),
     path("spotify/", views.spotify_view, name="Spotify"),
     path("tracks/", views.tracks_view, name="Tracks"),
     path("umap/", views.umap_view, name="UMAP"),
